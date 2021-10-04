ac2ff21ff18d31bd40b4f352135193b7ab669fb3
Valerian Saliou <valerian@valeriansaliou.name>
2021-10-04 11:20:41 -0500

    Lightify

--- gulpfile.js/paths.js
+++ gulpfile.js/paths.js
@@ -7,10 +7,6 @@ module.exports = {
 	themes: ['themes/*.css', '!themes/*.min.css'],
 	main: [
 		'components/prism-core.js',
-		'components/prism-markup.js',
-		'components/prism-css.js',
-		'components/prism-clike.js',
-		'components/prism-javascript.js',
 		'plugins/file-highlight/prism-file-highlight.js'
 	],
 	plugins: ['plugins/**/*.js', '!plugins/**/*.min.js'],
